c8d3c30db948b1e5d7c88783ea23c6de8a4ae79a
ashokdev <jobeditions@gmail.com>
2017-08-22 15:16:55 +0200

    Password routes add in web.php

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,12 @@ Route::prefix('/admin')->group(function(){
 Route::group(['middleware' => 'web'], function () {
 Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 Route::get('/', 'AdminController@index')->name('admin.dashboard');
+
+// Password reset routes
+  Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+  Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+  Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
 });
+
 });
+ 

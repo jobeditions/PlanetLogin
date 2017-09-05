d6828c217cce638a2dc21efaa4a71d117787d2bd
ashokdev <jobeditions@gmail.com>
2017-09-05 10:35:57 +0200

    Category Soft Deletes implemented

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+
 
 class Category extends Model
 {
-    
+    use SoftDeletes;
 	protected $fillable = [
         'name','title', 'order',
       ];
